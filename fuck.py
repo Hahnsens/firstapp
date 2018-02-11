68c621c4b8992649869bf104011f4bf79b4b432a
Hahnsens <2595180031@qq.com>
2018-02-11 17:11:49 +0800

    Initial commit

--- fuck.py
+++ fuck.py
@@ -2,8 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "It Works!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+def index():
+	return "fucks!"
+if __name__ == '__main__':
+	app.run(port=5000, debug=True)
